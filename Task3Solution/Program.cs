ce616ae2fe328a62c6a250457d4e921cad13e855
Salima Juma <salimaj16@icloud.com>
2026-07-11 01:27:43 +0400

    Problem 1

---
+++ Task3Solution/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task3Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			// Task 1 : Absolute Difference 
+			Console.Write("Enter the first number: ");
+			double firstNumber = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter the second number: ");
+			double secondNumber = double.Parse(Console.ReadLine());
+
+			double difference = firstNumber - secondNumber;
+			double positiveDifference = Math.Abs(difference);
+
+			Console.WriteLine("Positive Difference: " + positiveDifference);
+		}
+	}
+}
