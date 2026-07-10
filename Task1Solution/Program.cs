1a4fed181267f9411543c02ead5d2d422e870eea
Salima Juma <salimaj16@icloud.com>
2026-07-10 23:24:54 +0400

    add project files

---
+++ Task1Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task1Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
