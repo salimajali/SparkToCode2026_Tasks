99a28e5d9082efc02cdd25747e5435812915fcc3
Salima Juma <salimaj16@icloud.com>
2026-07-11 00:58:10 +0400

    Problem 1

---
+++ Task2Solution/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			// Task 1 : Countdown Timer
+			Console.Write("Enter a starting number: ");
+			int startingNumber = int.Parse(Console.ReadLine());
+
+			for (int counter = startingNumber; counter >= 1; counter--)
+			{
+				Console.WriteLine(counter);
+			}
+
+			Console.WriteLine("Liftoff!");
+
+
+		}
+	}
+}
