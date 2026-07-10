e28a284f721d24a61b40807753145d10b1aeab98
Salima Juma <salimaj16@icloud.com>
2026-07-11 00:09:22 +0400

    Problem 9 solution

--- Task1Solution/Program.cs
+++ Task1Solution/Program.cs
@@ -189,7 +189,45 @@
 				Console.WriteLine("Discount Amount: " + discountAmount + " OMR");
 				Console.WriteLine("Final Amount: " + finalAmount + " OMR");
 
+				//Task 9 : Day Name Finder
 
+				Console.Write("Enter a day number from 1 to 7: ");
+				int dayNumber = int.Parse(Console.ReadLine());
+
+				switch (dayNumber)
+				{
+					case 1:
+						Console.WriteLine("Sunday");
+						break;
+
+					case 2:
+						Console.WriteLine("Monday");
+						break;
+
+					case 3:
+						Console.WriteLine("Tuesday");
+						break;
+
+					case 4:
+						Console.WriteLine("Wednesday");
+						break;
+
+					case 5:
+						Console.WriteLine("Thursday");
+						break;
+
+					case 6:
+						Console.WriteLine("Friday");
+						break;
+
+					case 7:
+						Console.WriteLine("Saturday");
+						break;
+
+					default:
+						Console.WriteLine("Invalid day number");
+						break;
+				}
 
 
 
