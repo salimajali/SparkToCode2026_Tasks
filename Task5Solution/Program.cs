d72f32a36a751504f5c4c8c6ea72abd991154ccb
Salima Juma <salimaj16@icloud.com>
2026-07-11 22:29:45 +0400

    Problem 1

---
+++ Task5Solution/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task5Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			//Task 1 : Fixed Grades Array
+			int[] grades = new int[5];
+
+			// Enter grades using for loop
+			for (int counter = 0; counter < grades.Length; counter++)
+			{
+				Console.WriteLine("Enter grade number " + (counter + 1) + ":");
+				grades[counter] = int.Parse(Console.ReadLine());
+			}
+
+			// Print grades using foreach loop
+			Console.WriteLine("Student Grades:");
+
+			foreach (int grade in grades)
+			{
+				Console.WriteLine(grade);
+			}
+
+
+
+		}
+	}
+}
