03b6db9fc83b4c433b036f2df5a4aed9d5adfcd8
Salima Juma <salimaj16@icloud.com>
2026-07-11 22:30:00 +0400

    Problem 2

--- Task5Solution/Program.cs
+++ Task5Solution/Program.cs
@@ -22,6 +22,28 @@
 				Console.WriteLine(grade);
 			}
 
+			// Task 2 : Dynamic To-Do List
+			List<string> todoList = new List<string>();
+
+			for (int counter = 0; counter < 5; counter++)
+			{
+				Console.WriteLine("Enter task number " + (counter + 1) + ":");
+				string task = Console.ReadLine();
+
+				todoList.Add(task);
+			}
+
+			Console.WriteLine("To-Do List:");
+
+			int taskNumber = 1;
+
+			foreach (string task in todoList)
+			{
+				Console.WriteLine(taskNumber + ". " + task);
+				taskNumber++;
+			}
+
+
 
 
 		}
