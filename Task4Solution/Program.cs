e055cb065bce9d36905581c5150dd7a6f457835b
Salima Juma <salimaj16@icloud.com>
2026-07-11 18:36:42 +0400

    Problem 1

---
+++ Task4Solution/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4Solution
+{
+		internal class Program
+		{
+			public static void PrintWelcome(string name)
+			{
+				Console.WriteLine("Welcome to the application, " + name);
+			}
+
+			static void Main(string[] args)
+			{
+				Console.Write("Enter your name: ");
+				string userName = Console.ReadLine();
+
+				PrintWelcome(userName);
+			}
+		}
+	}
